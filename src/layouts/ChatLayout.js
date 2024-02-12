2eb37b28b15e5c1486f8f3ef53db9f80d3f8e088
jihoon-woo <jihoon9809@naver.com>
2024-02-13 02:04:16 +0900

    Feat ChatPortal complete

--- src/layouts/ChatLayout.js
+++ src/layouts/ChatLayout.js
@@ -1,11 +1,69 @@
 import "../pages/ChatBot/ChatPortal.css"
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import logo from "../components/logo.png";
-import Nav from 'react-bootstrap/Nav';
+import Modal from 'react-modal';
+import Nav_Search from "../components/nav_search.png"
+import Nav_Change_Language from "../components/nav_change_language.png"
+import Nav_Notice from "../components/nav_notice.png"
+import Nav_Profile from "../components/avatar.png"
+import Appointment from "../components/menu_appointment.png"
+import Calendar from "../components/menu_calendar.png"
+import Chatbot from "../components/menu_chatbot.png"
+import Homepage from "../components/menu_homepage.png"
+import Management from "../components/menu_management.png"
+import Mypage from "../components/menu_mypage.png"
+import Payments from "../components/menu_payments.png"
+import Record from "../components/menu_record.png"
 import Redbox from "../components/redbox.png"
 import Bluebox from "../components/bluebox.png"
 import Yellowbox from "../components/yellowbox.png"
+import Body_Arrow from "../components/arrow-right.png"
+import Demo from "../components/placeholder.jpg"
 
 const ChatLayout = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    
+    const modal_notice = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+    const ToggleContainer = styled.div`
+        position: relative;
+        > .toggle-container {
+            width: 50px;
+            height: 25px;
+            border-radius: 30px;
+            background-color: rgb(233,233,234);}
+        > .toggle--checked {
+            background-color: rgb(0,200,102);
+            transition : 0.5s
+        }
+        > .toggle-circle {
+            position: absolute;
+            top: 1px;
+            left: 1px;
+            width: 22px;
+            height: 23px;
+            border-radius: 50%;
+            background-color: rgb(255,254,255);
+            transition : 0.5s
+        } >.toggle--checked {
+            left: 27px;
+            transition : 0.5s
+        }`
+    ;
+    const [isOn, setisOn] = useState(false);
+    const toggleHandler = () => {
+        // isOn의 상태를 변경하는 메소드를 구현
+        setisOn(!isOn)
+    };
     return (
         <>
             <div className="header">
@@ -13,74 +71,183 @@ const ChatLayout = () => {
                     <img class="logo" src={logo}></img>
                 </div>
                 <div className="upper-nav">
-                    <h1> hello!</h1>
+                    <div className="nav_searchbox">
+                        <img class="search_logo" src={Nav_Search} />
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="nav_search_bar"
+                        />
+                    </div>
+                    <div className="nav_status">
+                        <ToggleContainer className="nav_button" onClick={toggleHandler}>
+                            <div className={`toggle-container ${isOn ? "toggle--checked" : null}`}/>
+                            <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}/>   
+                        </ToggleContainer>
+                        {isOn === false ?
+                        <div className='nav_off'>Off</div> :
+                        <div className='nav_live'>Live</div>}
+                    </div>
+                    <div className="nav_language_box">
+                        <div className="nav_language_select">
+                            English
+                        </div>
+                        <img class="nav_language_button" src={Nav_Change_Language} />
+                    </div>
+                    <div className="nav_notice">
+                        <img class="nav_notice_button" src={Nav_Notice} onClick={() => setModalIsOpen(true)} />
+                        <Modal style="modal_notice" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                            알림창 띄워줄께
+                        </Modal>
+                    </div>
+                    <div className="nav_login">
+                        <img class="nav_avatar" src={Nav_Profile} />
+                        <div className="nav_data">
+                            <div className="nav_nickname">
+                                User Name
+                            </div>
+                            <div className="nav_ID">
+                                ID: 1234567
+                            </div>
+                        </div>
+                        <img class="nav_profile_button" src={Nav_Change_Language} />
+                    </div>
                 </div>
             </div>
             <div className="main">
                 <div className="side-nav">
                     <div className="flex-column">
-                        <h1 className="pl-6 pr-6 text-4xl">Menu</h1>
+                        <h1 className="nav_menu">Menu</h1>
                         <div className="w-[100%] my-[1%] border-[1px] border-white"></div>
                         <div>
-                            <h2 className="p-6 text-4xl">Core</h2>
+                            <h2 className="nav_submenu">Core</h2>
                         </div>
 
                         <div className="nav-box">
-                            <a href="/" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Homepage} />
+                            <a href="/" className="nav_data">
                                 Home Page
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/mypage" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Mypage} />
+                            <a href="/mypage" className="nav_data">
                                 My Page
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/chatbot" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Chatbot} />
+                            <a href="/chatbot" className="nav_data">
                                 Chatbot
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/appointment" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Appointment} />
+                            <a href="/appointment" className="nav_data">
                                 Appointment
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/record" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Record} />
+                            <a href="/record" className="nav_data">
                                 Record
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/calendar" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Calendar} />
+                            <a href="/calendar" className="nav_data">
                                 Calendar
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="nav-box">
-                            <a href="/payments" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Payments} />
+                            <a href="/payments" className="nav_data">
                                 Payments
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                         <div className="w-[100%] my-[1%] border-[1px] border-white"></div>
                         <div>
-                            <h2 className="p-6 text-4xl">Commerce</h2>
+                            <h2 className="nav_submenu">Commerce</h2>
                         </div>
                         <div className="nav-box">
-                            <a href="/management" className="pr-6 text-2xl">
+                            <img class="nav_logo" src={Management} />
+                            <a href="/management" className="nav_data">
                                 Management
                             </a>
+                            <div className="nav_dash">
+                                &gt;
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="body">
                     <div className="boxes">
                         <div className="red-box">
-                            <img src={Redbox}></img>
+                            <img className="box-icon" src={Redbox}></img>
+                            <div className="box-text">
+                                상담내역 보기
+                            </div>
+                            <div className="box-to">
+                                <img class="box-arrow" src={Body_Arrow} />
+                            </div>
                         </div>
                         <div className="yellow-box">
-                            <img src={Yellowbox}></img>
+                            <img className="box-icon" src={Yellowbox}></img>
+                            <div className="box-text">
+                                일정관리
+                            </div>
+                            <div className="box-to">
+                                <img class="box-arrow" src={Body_Arrow} />
+                            </div>
                         </div>
                         <div className="blue-box">
-                            <img src={Bluebox}></img>
+                            <img className="box-icon" src={Bluebox}></img>
+                            <div className="box-text">
+                                환자관리
+                            </div>
+                            <div className="box-to">
+                                <img class="box-arrow" src={Body_Arrow} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="charts">
+                        <div className="chart-box-left">
+                            <div className="placeholder"> Chart </div>
+                        </div>
+                        <div className="chart-box-right">
+                            <div className="placeholder"> Chart </div>
+                        </div>
+                    </div>
+                    <div className="tables">
+                        <div className="table_container">
+                            <div className="table_name">
+                                XXX의 상담내역
+                            </div>
+                            <div className="table_sort">
+                                
+                            </div>
+                            <div className="placeholder"> Table </div>
                         </div>
                     </div>
                 </div>
@@ -89,6 +256,8 @@ const ChatLayout = () => {
         </>
     );
 }
+
+
 
 export default ChatLayout;
 
