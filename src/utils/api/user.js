6b766a6f13e730e73e70481a38d5bb22edcec6e7
vesselofgod <vesselofgod@naver.com>
2024-02-16 12:47:45 +0900

    feat: Add a user login api and auth provider

---
+++ src/utils/api/user.js
@@ -0,0 +1,43 @@
+//for API calls
+import axios from 'axios';
+const API_URL = "http://localhost:8080/";
+
+/*
+req: id, passwd
+res: id: [user의 id값], name: [user 성명]
+*/
+
+export const login = async ({ uid, password }) => {
+  return axios
+  .post(API_URL+'/login', { uid: uid, password: password })
+  .then((response) => {
+    console.log(response);
+    return response;
+  });
+}
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+/*
+req: name, id, passwd
+res: id: [user의 id값], name: [user 성명]
+*/
+export const register = async ({ name, id, passwd }) => {
+  return axios
+  .post(API_URL+'/register', { name: name, id: id, passwd: passwd })
+  .then((response) => {
+    console.log(response);
+    return response;
+  });
+}
+
+
+const UserService = {
+  register,
+  login,
+  getCurrentUser,
+}
+
+export default UserService;
