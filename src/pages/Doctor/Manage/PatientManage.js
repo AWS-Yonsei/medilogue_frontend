7bba1435480447a143d8ef76fdb6a8556de00acc
jihoon-woo <jihoon9809@naver.com>
2024-02-19 21:10:10 +0900

    Clean Doctor Folder

---
+++ src/pages/Doctor/Manage/PatientManage.js
@@ -0,0 +1,11 @@
+import PatientManageLayout from "./PatientManageLayout";
+
+const PatientManage = () => {
+    return(
+        <PatientManageLayout>
+        
+        </PatientManageLayout>
+    );
+}
+
+export default PatientManage;
